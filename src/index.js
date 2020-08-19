a48868f175fca3453a893d1dde7c753206567b1b
Noé Sánchez <noeszc@gmail.com>
2020-08-19 16:50:31 -0500

    reducing redux boilerplate with redux-toolkit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,35 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
 
-ReactDom.render(<div>App works</div>, document.getElementById('app'));
+import App from 'containers/App';
+import history from 'utils/history';
+import configureStore from './configureStore';
+
+console.log(history);
+
+// Create redux store with history
+const initialState = {};
+const store = configureStore(initialState, history);
+const MOUNT_NODE = document.getElementById('app');
+console.log(store.getState());
+
+const ConnectedApp = ({ Component }) => (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <React.StrictMode>
+        <Component></Component>
+      </React.StrictMode>
+    </ConnectedRouter>
+  </Provider>
+);
+
+const render = (Component) => {
+  ReactDom.render(
+    <ConnectedApp Component={Component}></ConnectedApp>,
+    MOUNT_NODE,
+  );
+};
+
+render(App);
