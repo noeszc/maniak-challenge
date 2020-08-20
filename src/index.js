d1da27546c24f1597ff0d27bbf16483db421d856
Noé Sánchez <noeszc@gmail.com>
2020-08-19 22:05:05 -0500

    add a fixture api client

--- src/index.js
+++ src/index.js
@@ -7,13 +7,10 @@ import App from 'containers/App';
 import history from 'utils/history';
 import configureStore from './configureStore';
 
-console.log(history);
-
 // Create redux store with history
 const initialState = {};
 const store = configureStore(initialState, history);
 const MOUNT_NODE = document.getElementById('app');
-console.log(store.getState());
 
 const ConnectedApp = ({ Component }) => (
   <Provider store={store}>
