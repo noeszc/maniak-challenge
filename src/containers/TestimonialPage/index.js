c0a5da4ea7fcc57841cf62b42a511562ded5bd80
Noé Sánchez <noeszc@gmail.com>
2020-08-25 03:10:35 -0500

    add testimonial page with reviews

--- src/containers/TestimonialPage/index.js
+++ src/containers/TestimonialPage/index.js
@@ -1,13 +1,25 @@
+import _ from 'lodash';
 import React from 'react';
+import { Spinner, Box, Flex } from '@chakra-ui/core';
+
 import useLoadPage from 'containers/PagesManager/components/useLoadPage';
+import H1 from 'components/elements/H1';
+import Rotator from './Rotator';
 
 function TestimonialPage() {
-  const state = useLoadPage();
+  const { loading, value } = useLoadPage();
+
+  if (loading || _.isEmpty(value)) {
+    return <Spinner alignSelf="center" m="auto"></Spinner>;
+  }
+
+  const items = _.get(value, 'slider.reviews');
+
   return (
-    <div>
-      <h1>TestimonialPage</h1>
-      <pre>{JSON.stringify(state, null, 2)}</pre>
-    </div>
+    <Box paddingTop={32}>
+      <H1>{_.get(value, 'slider.title')}</H1>
+      <Rotator mt={20} items={items} max={items.length - 1}></Rotator>
+    </Box>
   );
 }
 
